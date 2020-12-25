2b8bba680d3e1eec9e237a66859a2856038d60b8
Lili Fuentes <lile7886@gmail.com>
2020-12-25 17:55:02 -0500

    changed server to heroku

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lili@localhost/meals',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://lili@localhost/meals'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://umaaznjedfinhc:a79f0a8df16cb71b6b52b1cb9f6989b353067a17ea2bc5b690438aecef6d756e@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d4bep6dih2u8j0',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://umaaznjedfinhc:a79f0a8df16cb71b6b52b1cb9f6989b353067a17ea2bc5b690438aecef6d756e@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d4bep6dih2u8j0'
 }
 
 // postgres://umaaznjedfinhc:a79f0a8df16cb71b6b52b1cb9f6989b353067a17ea2bc5b690438aecef6d756e@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d4bep6dih2u8j0
+
+// postgresql://lili@localhost/meals
