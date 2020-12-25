e4a4120255611f51ca8ee813191765112d10ff9c
Lili Fuentes <lile7886@gmail.com>
2020-12-25 18:39:48 -0500

    reinstalled postgrator

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ module.exports = {
     PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://umaaznjedfinhc:a79f0a8df16cb71b6b52b1cb9f6989b353067a17ea2bc5b690438aecef6d756e@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d4bep6dih2u8j0',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://umaaznjedfinhc:a79f0a8df16cb71b6b52b1cb9f6989b353067a17ea2bc5b690438aecef6d756e@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d4bep6dih2u8j0'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://oderdmjkmivvjz:11c0d01390f5d6e69f768b2bc4cd2ad86eb2f09b1979a918e1a2ef43f62df170@ec2-3-224-38-18.compute-1.amazonaws.com:5432/dd086b5ocq93ld',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://oderdmjkmivvjz:11c0d01390f5d6e69f768b2bc4cd2ad86eb2f09b1979a918e1a2ef43f62df170@ec2-3-224-38-18.compute-1.amazonaws.com:5432/dd086b5ocq93ld'
 }
 
-// postgres://umaaznjedfinhc:a79f0a8df16cb71b6b52b1cb9f6989b353067a17ea2bc5b690438aecef6d756e@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d4bep6dih2u8j0
+// postgres://oderdmjkmivvjz:11c0d01390f5d6e69f768b2bc4cd2ad86eb2f09b1979a918e1a2ef43f62df170@ec2-3-224-38-18.compute-1.amazonaws.com:5432/dd086b5ocq93ld
 
 // postgresql://lili@localhost/meals
