4f74154ecb1b014e938ecc95f46103a6ff78495e
Lili Fuentes <lile7886@gmail.com>
2020-12-25 13:46:07 -0500

    chefs api

--- seeds/seed.chefs.sql
+++ seeds/seed.chefs.sql
@@ -4,27 +4,27 @@ INSERT INTO chefs
     (chef_name, bio, cuisine, schedule)
 VALUES
 ('Johnathan', 
-'Fake BIO goes here.',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus nulla at. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Risus quis varius quam quisque id. Nunc id cursus metus aliquam eleifend mi in. Adipiscing at in tellus integer. Arcu non sodales neque sodales. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Massa tincidunt dui ut ornare. Tempor id eu nisl nunc mi ipsum. Urna cursus eget nunc scelerisque viverra. Sem viverra aliquet eget sit amet. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Velit laoreet id donec ultrices. Mattis aliquam faucibus purus in massa tempor. Dignissim enim sit amet venenatis.',
 ('American', 'Asian', 'Greek', 'Indian', 'Latin American', 'Mexican', 'Middle Eastern', 'Spanish', 'Thai'),
 ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')),
 
 ('Francesca', 
-'Fake BIO goes here.',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus nulla at. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Risus quis varius quam quisque id. Nunc id cursus metus aliquam eleifend mi in. Adipiscing at in tellus integer. Arcu non sodales neque sodales. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Massa tincidunt dui ut ornare. Tempor id eu nisl nunc mi ipsum. Urna cursus eget nunc scelerisque viverra. Sem viverra aliquet eget sit amet. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Velit laoreet id donec ultrices. Mattis aliquam faucibus purus in massa tempor. Dignissim enim sit amet venenatis.',
 ('Asian', 'Greek', 'Italian', 'Latin American', 'Mexican', 'Spanish'),
 ('Monday', 'Tuesday', 'Saturday', 'Sunday')),
 
 ('Christian', 
-'Fake BIO goes here.',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus nulla at. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Risus quis varius quam quisque id. Nunc id cursus metus aliquam eleifend mi in. Adipiscing at in tellus integer. Arcu non sodales neque sodales. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Massa tincidunt dui ut ornare. Tempor id eu nisl nunc mi ipsum. Urna cursus eget nunc scelerisque viverra. Sem viverra aliquet eget sit amet. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Velit laoreet id donec ultrices. Mattis aliquam faucibus purus in massa tempor. Dignissim enim sit amet venenatis.',
 ('American', 'Asian', 'French', 'Indian', 'Mexican', 'Spanish'),
 ('Thursday', 'Friday', 'Saturday', 'Sunday')),
 
 ('Madelyn', 
-'Fake BIO goes here.',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus nulla at. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Risus quis varius quam quisque id. Nunc id cursus metus aliquam eleifend mi in. Adipiscing at in tellus integer. Arcu non sodales neque sodales. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Massa tincidunt dui ut ornare. Tempor id eu nisl nunc mi ipsum. Urna cursus eget nunc scelerisque viverra. Sem viverra aliquet eget sit amet. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Velit laoreet id donec ultrices. Mattis aliquam faucibus purus in massa tempor. Dignissim enim sit amet venenatis.',
 ('American', 'Asian', 'Greek', 'Indian', 'Mexican', 'Thai'),
 ('Monday', 'Tuesday', 'Wednesday', 'Sunday')),
 
 ('Joaquin', 
-'Fake BIO goes here.',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus nulla at. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Risus quis varius quam quisque id. Nunc id cursus metus aliquam eleifend mi in. Adipiscing at in tellus integer. Arcu non sodales neque sodales. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Massa tincidunt dui ut ornare. Tempor id eu nisl nunc mi ipsum. Urna cursus eget nunc scelerisque viverra. Sem viverra aliquet eget sit amet. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Velit laoreet id donec ultrices. Mattis aliquam faucibus purus in massa tempor. Dignissim enim sit amet venenatis.',
 ('French', 'Greek', 'Italian', 'Latin American', 'Mexican', 'Middle Eastern', 'Thai'),
 ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'));
 
