5917b276c76aaa4e62521d552e5160ad3ae3e726
Lili Fuentes <lile7886@gmail.com>
2020-12-29 12:15:06 -0500

    changed database url

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lili@localhost/meals',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://lili@localhost/meals'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll'
 }
 
-// postgres://oderdmjkmivvjz:11c0d01390f5d6e69f768b2bc4cd2ad86eb2f09b1979a918e1a2ef43f62df170@ec2-3-224-38-18.compute-1.amazonaws.com:5432/dd086b5ocq93ld
+// postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll
 
 // postgresql://lili@localhost/meals
