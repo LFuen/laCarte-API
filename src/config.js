f73ea1ebe5b0cbeaa2dc5078d1effc15c6ae96b5
Lili Fuentes <lile7886@gmail.com>
2020-12-29 15:14:17 -0500

    heroku changes

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     API_TOKEN: process.env.API_TOKEN,
     DATABASE_URL: process.env.DATABASE_URL || 'postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://la-carte.lfuen.vercel.app/'
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://la-carte.lfuen.vercel.app'
 }
 
 // postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll
