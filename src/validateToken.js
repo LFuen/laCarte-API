8f585495141d788bf4baea3678e40767b3396d51
Lili Fuentes <lile7886@gmail.com>
2021-04-22 16:17:21 -0400

    add console log

--- src/validateToken.js
+++ src/validateToken.js
@@ -1,8 +1,12 @@
+const { Console } = require("winston/lib/winston/transports");
 const logger = require("./logger");
 
 function validateToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get(`Authorization`);
+
+  console.log(apiToken, 'API')
+  console.log(authToken, "AUTH")
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
