6fe1c2b29a4cd4638f9a0f2b0cafe76e3cfef533
Lili Fuentes <lile7886@gmail.com>
2021-04-22 13:26:21 -0400

    changed origin because of vercel . to -

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL || "postgresql://lili@localhost/meals",
   CLIENT_ORIGIN:
-    process.env.CLIENT_ORIGIN || "https://la-carte.lfuen.vercel.app",
+    process.env.CLIENT_ORIGIN || "https://la-carte-lfuen.vercel.app",
 };
 
 // postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll
