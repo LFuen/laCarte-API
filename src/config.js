27095cf3fe4a4d3462d902614d098dc293cf98e4
Lili Fuentes <lile7886@gmail.com>
2021-04-22 13:40:57 -0400

    changed auth key

--- src/config.js
+++ src/config.js
@@ -14,3 +14,5 @@ module.exports = {
 // postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll
 
 // postgresql://lili@localhost/meals
+
+//changed auth key 4/22/21
