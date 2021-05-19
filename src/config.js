0cc7e49d6dfa9e981eb57ab2ee3da51170e5456c
Lili <lile7886@gmail.com>
2021-05-19 19:24:10 -0400

    changed herkou app

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   API_TOKEN: process.env.API_TOKEN,
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgres://lxrskbiarhkudb:9b49de24d89d2474475a797fbfb140f05bb6be0e115a336730f5216460282982@ec2-34-196-34-158.compute-1.amazonaws.com:5432/d5njamcr3h74ll",
+    "postgres://zussweygrlrnbd:613aafecc17b082c0dbda029a253e5bc976267dafb4a31fe8e83e9b31a22e2a1@ec2-34-235-240-133.compute-1.amazonaws.com:5432/d98u3n95e5h16",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL || "postgresql://lili@localhost/meals",
   CLIENT_ORIGIN:
